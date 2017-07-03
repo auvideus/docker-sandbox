5547a529f3827e0c3732f34ae9012e5c2d9e909e
auvideus <eric.august.huneke@protonmail.com>
2017-07-03 18:09:24 -0500

    Add &&s to run script

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Stopping sandbox"
-docker rm -f sandbox
-echo "Building sandbox"
-docker build --tag sandbox:latest .
-echo "Running sandbox"
-docker run -dt -v d:/no-sync/dev:/opt/dev --rm --name sandbox -h localhost sandbox:latest
+echo "Stopping sandbox" && \
+docker rm -f sandbox && \
+echo "Building sandbox" && \
+docker build --tag sandbox:latest . && \
+echo "Running sandbox" && \
+docker run -dt -v d:/no-sync/dev:/opt/dev --rm --name sandbox -h localhost sandbox:latest && \
 winpty docker exec -it -u root sandbox fish
