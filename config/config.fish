071431d70b6d668fe38c64ebb50fbc54e40f3a7a
auvideus <eric.august.huneke@protonmail.com>
2017-07-04 14:29:29 -0500

    workking digital ocean connectivity

--- config/config.fish
+++ config/config.fish
@@ -1,1 +1,6 @@
+mkdir -p -m 700 /root/.ssh
+cp /opt/sdev/security/id_rsa /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+cp /opt/sdev/security/id_rsa.pub /root/.ssh/id_rsa.pub
+chmod 666 /root/.ssh/id_rsa.pub
 cd /opt/dev/repos
