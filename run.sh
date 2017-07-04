dc9c1597cc65ac08803463ff1404ce7b0e1fdb1a
auvideus <eric.august.huneke@protonmail.com>
2017-07-03 19:06:43 -0500

    add /root as volume

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ docker rm -f sandbox && \
 echo "Building sandbox" && \
 docker build --tag sandbox:latest . && \
 echo "Running sandbox" && \
-docker run -dt -v d:/no-sync/dev:/opt/dev --rm --name sandbox -h localhost sandbox:latest && \
+docker run -dt -v roothome:/root -v d:/no-sync/dev:/opt/dev --rm --name sandbox -h localhost sandbox:latest && \
 winpty docker exec -it -u root sandbox fish
