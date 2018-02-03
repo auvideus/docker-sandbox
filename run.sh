02f62d0bc4b606bf89ed6c06120a3cc5353252b8
auvideus <eric.august.huneke@protonmail.com>
2018-02-03 11:00:22 -0600

    attach windows socket to container

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ docker rm -f sandbox
 echo "Building sandbox" && \
 docker build --tag sandbox:latest . && \
 echo "Running sandbox" && \
-docker run -dt -v d:/sync-home/dev/docker:/opt/sdev -v d:/no-sync/dev/docker:/opt/dev --rm --name sandbox -h localhost sandbox:latest && \
+docker run -dt -v d:/sync-home/dev/docker:/opt/sdev -v d:/no-sync/dev/docker:/opt/dev -v //var/run/docker.sock:/var/run/docker.sock --rm --name sandbox -h localhost sandbox:latest && \
 winpty docker exec -it -u root sandbox bash -c 'cd /opt/dev/repos && fish'
