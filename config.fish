b12fdfa75a13c6b0b8db65d23c1ad40b469005bd
auvideus <eric.huneke@protonmail.com>
2018-02-13 16:49:50 -0600

    fix git credentials by setting config individually

--- config.fish
+++ config.fish
@@ -4,7 +4,12 @@ chmod 600 /root/.ssh/id_rsa
 cp /opt/security/id_rsa.pub /root/.ssh/id_rsa.pub
 chmod 666 /root/.ssh/id_rsa.pub
 
-cp /opt/home/.gitconfig /root/.gitconfig
+set GIT_NAME (git config -f /opt/home/.gitconfig --get user.name)
+set GIT_EMAIL (git config -f /opt/home/.gitconfig --get user.email)
+git config --global user.name $GIT_NAME
+git config --global user.email $GIT_EMAIL
+git config credential.helper store
+
 cp /opt/security/.git-credentials /root/.git-credentials
 
 set -x GOPATH /opt/dev/gopath
