135d7e3403b835a5ed9e2b22814a68802c15ea39
auvideus <eric.huneke@protonmail.com>
2018-02-19 03:40:23 -0600

    remove gobin

--- config.fish
+++ config.fish
@@ -16,7 +16,6 @@ mkdir -p /root/.local/share/fish
 ln -s /opt/home/.local/share/fish/fish_history /root/.local/share/fish/fish_history
 
 set -x GOPATH /opt/dev/gopath
-set -x GOBIN /opt/dev/gopath/bin
 
 set PATH $GOPATH/bin /opt/web $PATH
 
