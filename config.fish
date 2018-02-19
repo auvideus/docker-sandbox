11d2c6aa80977f5562cc443d02785b53946d71f8
auvideus <eric.huneke@protonmail.com>
2018-02-19 04:40:42 -0600

    run fish config only in login

--- config.fish
+++ config.fish
@@ -1,22 +1,27 @@
-mkdir -p -m 700 /root/.ssh
-cp /opt/security/id_rsa /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
-cp /opt/security/id_rsa.pub /root/.ssh/id_rsa.pub
-chmod 666 /root/.ssh/id_rsa.pub
+if status --is-login
+    mkdir -p -m 700 /root/.ssh
+    cp /opt/security/id_rsa /root/.ssh/id_rsa
+    chmod 600 /root/.ssh/id_rsa
+    cp /opt/security/id_rsa.pub /root/.ssh/id_rsa.pub
+    chmod 666 /root/.ssh/id_rsa.pub
 
-set GIT_NAME (git config -f /opt/home/.gitconfig --get user.name)
-set GIT_EMAIL (git config -f /opt/home/.gitconfig --get user.email)
-git config --global user.name $GIT_NAME
-git config --global user.email $GIT_EMAIL
-git config --global credential.helper store
+    set -l GIT_NAME (git config -f /opt/home/.gitconfig --get user.name)
+    set -l GIT_EMAIL (git config -f /opt/home/.gitconfig --get user.email)
+    git config --global user.name $GIT_NAME
+    git config --global user.email $GIT_EMAIL
+    git config --global credential.helper store
 
-cp /opt/security/.git-credentials /root/.git-credentials
+    cp /opt/security/.git-credentials /root/.git-credentials
 
-mkdir -p /root/.local/share/fish
-ln -s /opt/home/.local/share/fish/fish_history /root/.local/share/fish/fish_history
+    mkdir -p /root/.local/share/fish
 
-set -x GOPATH /opt/dev/gopath
+    set -x GOPATH /opt/dev/gopath
 
-set PATH $GOPATH/bin /opt/web $PATH
+    ln --symbolic --force \
+        /opt/home/.local/share/fish/fish_history \
+        /root/.local/share/fish/fish_history
 
-alias do "doctl -t (cat /opt/security/digital_ocean_token)"
+    alias do "doctl -t (cat /opt/security/digital_ocean_token)"
+
+    set -x PATH $GOPATH/bin /opt/web $PATH
+end
