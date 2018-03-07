e11a305974e002f042bfbba0653992cad2195585
auvideus <eric.huneke@protonmail.com>
2018-03-07 16:45:51 -0600

    run.sh increase verbosity

--- run.sh
+++ run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-echo -e "\n----> BUILDING SANDBOX ENVIRONMENT\n"
+echo -e "\n----> BUILDING SANDBOX CONTAINER\n"
 
 docker build \
-    --quiet \
     --build-arg user=$USERNAME \
     --tag sandbox:latest \
     .
 
-echo -e "\n----> STARTING SANDBOX ENVIRONMENT\n"
+echo -e "\n----> RUNNING SANDBOX CONTAINER\n"
 
 docker run \
     --name sandbox \
@@ -23,8 +22,11 @@ docker run \
     -e SANDBOX_REPO_NAME=$SANDBOX_REPO_NAME \
     -v //var/run/docker.sock:/var/run/docker.sock \
     sandbox:latest
+
+echo -e "\n----> SHOWING CONTAINER OUTPUT\n"
+
 docker logs sandbox
 
-echo -e "\n----> RUNNING SANDBOX ENVIRONMENT\n"
+echo -e "\n----> ATTACHING TO SANDBOX CONTAINER\n"
 
 winpty docker attach sandbox
